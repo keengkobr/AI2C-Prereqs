01d9c9cdb376bdc7fd02f66d4e52abd310485db9
shafe123 <ethan.h.shafer@gmail.com>
2025-11-21 11:55:57 -0500

    final changes for container lesson

--- Misc Topics/Containers and Images/commands.sh
+++ Misc Topics/Containers and Images/commands.sh
@@ -13,3 +13,8 @@ docker container list
 
 docker container exec -it <container_name> /bin/sh
 docker inspect --format "{{json .State.Health }}" <container_name> | jq
+
+docker image save -o busybox.tar busybox
+tar tvf busybox.tar
+tar xf busybox.tar -C busybox/
+cat manifest.json
